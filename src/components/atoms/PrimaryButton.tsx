66d4f53c6f3f504a9950b6aded5909a9aed43942
rewdaraw <rewdaraw@gmail.com>
2021-05-09 18:41:38 +0900

    create login function

--- src/components/atoms/PrimaryButton.tsx
+++ src/components/atoms/PrimaryButton.tsx
@@ -1,16 +1,25 @@
 import React, { memo, ReactNode, VFC } from "react";
 import { Button } from "@chakra-ui/button";
 
-
 type Props = {
   children: ReactNode;
+  onClick: () => void;
+  loading?: boolean;
+  disabled?: boolean;
 };
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-  const { children } = props;
+  const { children, onClick, loading = false, disabled = false } = props;
 
   return (
-    <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+    <Button
+      bg="teal.400"
+      color="white"
+      _hover={{ opacity: 0.8 }}
+      onClick={onClick}
+      isLoading={loading}
+      disabled={disabled || loading}
+    >
       {children}
     </Button>
   );
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,10 +1,18 @@
-import React, { memo, VFC } from "react";
+import React, { ChangeEvent, memo, useState, VFC } from "react";
 import { Box, Divider, Flex, Heading, Stack } from "@chakra-ui/layout";
 import { Input } from "@chakra-ui/input";
 
 import { PrimaryButton } from "../atoms/PrimaryButton";
+import { useAuth } from "../../hooks/useAuth";
 
 export const Login: VFC = memo(() => {
+  const [userId, setUserId] = useState("");
+  const { login, loading } = useAuth();
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
+  const onClickLogin = () => login(userId);
+
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -13,8 +21,19 @@ export const Login: VFC = memo(() => {
         </Heading>
         <Divider my={4} />
         <Stack spacing={8} py={4} px={10}>
-          <Input placeholder="ユーザーID" />
-          <PrimaryButton>ログイン</PrimaryButton>
+          <Input
+            placeholder="ユーザーID"
+            borderColor="gray.200"
+            _placeholder={{ color: "gray.200" }}
+            onChange={onChangeUserId}
+          />
+          <PrimaryButton
+            onClick={onClickLogin}
+            loading={loading}
+            disabled={!userId}
+          >
+            ログイン
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>
