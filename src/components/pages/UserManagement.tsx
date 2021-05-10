9769ccc79477b7c7b328cc7e54a25f7708063d20
rewdaraw <rewdaraw@gmail.com>
2021-05-10 14:06:35 +0900

    create UserManagement component

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,14 +1,35 @@
-import React, { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/layout";
+import React, { memo, useEffect, VFC } from "react";
+import { Center, Wrap, WrapItem } from "@chakra-ui/layout";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { Spinner } from "@chakra-ui/spinner";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard imageUrl="https://source.unsplash.com/photos/random" userName="Toyo" fullName="Taylor Toyo" />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center" spacing="30px">
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/photos/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
